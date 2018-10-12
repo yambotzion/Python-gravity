55da18d412cbef790df0f6417177c25c2412f50a
yambotzion <44095061+yambotzion@users.noreply.github.com>
2018-10-12 08:54:21 -0700

    Changed Variables, spaced out variables

--- gravity.py
+++ gravity.py
@@ -1,15 +1,18 @@
+
 #October-12-2018
 
+GRAVITY = 9.8
+HEIGHT_0 = 16
 def calcFallTime(t, v, h):
-    h=-16*t*t+v*t+h
+    h=GRAVITY/2*t*t+v*t+h
     return h
 
 
 print "Calculate the height you will achieve"
 
-time=0.6 
-velocity=16
-height=16
+time = 0.6 
+velocity = 16
+height = HEIGHT_0
 
 final=calcFallTime(time, velocity, height)
 print "After 1.6 seconds with a starting"
